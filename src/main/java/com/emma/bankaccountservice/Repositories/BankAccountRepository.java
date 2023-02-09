413db98c808214b07c42185d849245763d95351c
Emmanuel Samandoulougou <samabdoulougoue@gmail.com>
2023-02-09 16:53:19 +0000

    Using of Spring Rest Ressource to create another endoint

--- src/main/java/com/emma/bankaccountservice/Repositories/BankAccountRepository.java
+++ src/main/java/com/emma/bankaccountservice/Repositories/BankAccountRepository.java
@@ -2,7 +2,9 @@ package com.emma.bankaccountservice.Repositories;
 
 import com.emma.bankaccountservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
 
 }
